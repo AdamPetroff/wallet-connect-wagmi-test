ae02798bac1ba3ef95581964074bea32703713fb
Adam Petroff <adampetroff11235@gmail.com>
2023-02-28 12:59:50 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Button, Web3Modal, Web3NetworkSwitch } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  arbitrum,
+  avalanche,
+  bsc,
+  fantom,
+  gnosis,
+  mainnet,
+  optimism,
+  polygon,
+} from "wagmi/chains";
+
+// 1. Get projectID at https://cloud.walletconnect.com
+const projectId = "9a5fbacd6fe4bbba1b1bcf56a6f0ef89";
+
+// 2. Configure wagmi client
+const chains = [
+  mainnet,
+  polygon,
+  avalanche,
+  arbitrum,
+  gnosis,
+  bsc,
+  optimism,
+  fantom,
+];
+
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    version: "1",
+    appName: "web3Modal",
+    chains,
+    projectId,
+  }),
+  provider,
+});
+
+// 3. Configure modal ethereum client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+// 4. Wrap your app with WagmiProvider and add <Web3Modal /> compoennt
+export default function App() {
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Web3Button />
+        <br />
+        <Web3NetworkSwitch />
+        <br />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
+}
